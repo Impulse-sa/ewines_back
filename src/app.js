67f80691c413cbe1c244be4ed65ad86d97c7fb29
Ignacio Cunial <igsecu@hotmail.com>
2022-10-19 23:29:43 -0300

    changes

--- src/app.js
+++ src/app.js
@@ -17,8 +17,8 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
 server.use(bodyParser.json({ limit: '50mb' }))
 server.use(morgan('dev'))
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000') // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header('Access-Control-Allow-Origin', req.headers.origin) // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', true)
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
@@ -37,7 +37,6 @@ server.use(
     secret: 'secretcode',
     resave: false,
     saveUninitialized: true,
-
     cookie: {
       httpOnly: true,
       secure: true,
