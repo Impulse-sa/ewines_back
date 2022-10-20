7e56622f76d03459c21a41af10e70a0a61ff73e0
Ignacio Cunial <igsecu@hotmail.com>
2022-10-19 22:22:14 -0300

    changes

--- src/app.js
+++ src/app.js
@@ -34,11 +34,11 @@ server.use(
     name: 'e-wine',
     secret: 'secretcode',
     resave: true,
-    saveUninitialized: true
-    /* cookie: {
-      sameSite: 'strict',
+    saveUninitialized: true,
+    cookie: {
+      sameSite: 'none',
       maxAge: 24 * 60 * 60 * 1000
-    } */
+    }
   })
 )
 
