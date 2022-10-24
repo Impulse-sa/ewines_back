51f855331be531a1a25fd1e850be1c23f54e44fd
Ignacio Cunial <igsecu@hotmail.com>
2022-10-23 21:58:01 -0300

    changes

--- src/routes/webhooksMPRoutes.js
+++ src/routes/webhooksMPRoutes.js
@@ -2,9 +2,9 @@ const server = require('express').Router()
 const { createBuy } = require('../controllers/webhooksMP')
 
 server.post('/', (req, res, next) => {
-  const id = req.body.data.id
-  const payment = req.query.action
-  if (id !== undefined && payment === 'payment') {
+  const id = req.content.data.id
+  const payment = req.content.action
+  if (id !== undefined && payment === 'payment.created') {
     try {
       const newBuy = createBuy(id)
       if (newBuy) {
