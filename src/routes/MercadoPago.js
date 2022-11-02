d2f3abc83eec179c29ea1c952debb8030ab4fe96
Ignacio Cunial <igsecu@hotmail.com>
2022-11-02 14:49:17 -0300

    changes

--- src/routes/MercadoPago.js
+++ src/routes/MercadoPago.js
@@ -22,9 +22,9 @@ server.post('/', (req, res) => {
 
     items: req.body.map(item => { return { ...item, currency_id: 'ARS' } }),
     back_urls: {
-      success: 'https://e-wine-ashen.vercel.app/userPurchased/',
-      failure: 'https://e-wine-ashen.vercel.app',
-      pending: 'https://e-wine-ashen.vercel.app'
+      success: 'http://localhost:3000/userPurchased/',
+      failure: 'http://localhost:3000',
+      pending: 'http://localhost:3000'
     },
     notification_url: 'https://e-winespf.herokuapp.com/notificacion'
   }
